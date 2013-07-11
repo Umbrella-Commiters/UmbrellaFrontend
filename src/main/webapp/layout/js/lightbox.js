26cb6f2edee8bee519e679c8f710af98d2694b6b
flowedback <bjoern.abt@object.ch>
2013-07-11 13:02:31 +0200

    fixed loading of an external image

--- src/main/webapp/layout/js/lightbox.js
+++ src/main/webapp/layout/js/lightbox.js
@@ -48,8 +48,8 @@ lightbox = new Lightbox options
   LightboxOptions = (function() {
 
     function LightboxOptions() {
-      this.fileLoadingImage = '/images/loading.gif';
-      this.fileCloseImage = '/images/close.png';
+      this.fileLoadingImage = '/euu/layout/images/loading.gif';
+      this.fileCloseImage = '/euu/layout/images/close.png';
       this.resizeDuration = 700;
       this.fadeDuration = 500;
       this.labelImage = "Image";
